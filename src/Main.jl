3094b5c782f5c4d651976fffcc23c233c525c907
Grzegorz Klosinski <g.m.klosinski.wut@gmail.com>
2026-08-05 22:28:50 +0200

    Refactor

--- src/Main.jl
+++ src/Main.jl
@@ -1,7 +1,7 @@
 using HTTP
 
-function hello_world(req::HTTP.Request)
-    return HTTP.Response(200, "Funny joke, isn't it?")
+function get_random_joke(req::HTTP.Request)
+    return HTTP.Response(200, "Why don't scientists trust atoms? Because they make up everything!")
 end
 
-HTTP.serve(hello_world, "0.0.0.0", 8080)
+HTTP.serve(get_random_joke, "0.0.0.0", 8080)
